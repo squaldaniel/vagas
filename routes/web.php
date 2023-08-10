ac2153222293de28842370dfa6010ae451dd90d1
squaldaniel <daniel.santos.ap@gmail.com>
2023-08-09 23:01:05 -0300

    muda php htaccess

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('adminlte.index');
+    return view('adminlte.bodyteste');
 });
 Route::get('register', function(){
    return view('adminlte.pages.examples.register-v2');
